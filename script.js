ed6c1e28f94740fd767e08ab68745f7cfb07777d
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-06-23 00:12:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function getSize(){
+    var w = window.innerWidth;
+	var h = window.innerHeight;
+
+	document.getElementById("sizeInfo").innerHTML="<h1> width: "+w+" and height: "+h+"</h1"
+}
